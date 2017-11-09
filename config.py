30e8e814521c6958c0ed1369a7c9bf4c8ed5bd26
xiemingsen <674733146@qq.com>
2017-11-09 22:29:59 +0800

    修改config

--- config.py
+++ config.py
@@ -2,12 +2,4 @@ from oslo_config import cfg
 
 CONF = cfg.CONF
 
-opts = [
-	cfg.StrOpt('test',
-               default='test',
-               help='Test'),
-	]
-
-CONF.register_opts(opts)
-
 CONF(['--config-file', '~/agent/config.conf'])
